919ae828bd6c83a26a9eb36b4ae9bf95b6819d6f
dobrosav22 <dobrosav.djukic@gmail.com>
2025-05-24 17:24:34 +0200

    envs

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,9 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.SUPABASE_URL || "";
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY || "";
-const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY || "";
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || "";
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY || "";
+const supabaseServiceRoleKey =
+  process.env.REACT_APP_SUPABASE_SERVICE_ROLE_KEY || "";
 
+console.log(process);
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceRoleKey);
